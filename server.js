de275251bc2564535166641520c46123a8a12d3b
dayuoba <taddayu@sina.com>
2015-04-21 22:11:53 +0800

    add application API

---
+++ server.js
@@ -0,0 +1,13 @@
+var restfulServer = require('./lib/jrest');
+var config = require('./config/config.json');
+
+restfulServer.run(config);
+
+process.on('uncaughtException', handleUncaughtException);
+
+function handleUncaughtException(err) {
+	//TODO
+	//restfulServer.application.NElogger.error(err);
+}
+
+
