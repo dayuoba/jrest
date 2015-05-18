f54ac5bb8302d3cab5a01698f122fd4253923666
dayuoba <taddayu@sina.com>
2015-05-18 22:11:24 +0800

    trying vim for editing

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ restfulServer.run(config);
 
 process.on('uncaughtException', handleUncaughtException);
 
+process.on('exit', function() {
+	console.log('process exited');
+	//register atExit() here
+});
+
 function handleUncaughtException(err) {
 	//TODO
 	//restfulServer.application.NElogger.error(err);
 	console.log(err.stack);
+	process.exit(0)
+	
 }
 
 
--- test/mongoConnection.js
+++ test/mongoConnection.js
@@ -9,4 +9,32 @@ describe('mongodb connection', function() {
 			done();
 		});
 	});
+	it('it should connect whitout error', function(done) {
+		mongodb.connect('mongodb://localhost:27017', function listener(err, db) {
+			should.not.exists(err);
+			// console.log(err)
+			done();
+		});
+	});
+	it('it should connect whitout error', function(done) {
+		mongodb.connect('mongodb://localhost:27017', function listener(err, db) {
+			should.not.exists(err);
+			// console.log(err)
+			done();
+		});
+	});
+	it('it should connect whitout error', function(done) {
+		mongodb.connect('mongodb://localhost:27017', function listener(err, db) {
+			should.not.exists(err);
+			// console.log(err)
+			done();
+		});
+	});
+	
+	it('should wait for 10s', function(done) {
+		this.timeout(11000);
+		setTimeout(function() {
+			done();
+		}, 10000);
+	});
 });
